1f62cc9b52a9dc7694fe5ad2d6f001db0e660af0
nzoliii <nemethzoltanjozsef@gmail.com>
2024-04-20 19:43:49 +0200

    Gerald memed

--- src/main/java/com/hbm/lib/RefStrings.java
+++ src/main/java/com/hbm/lib/RefStrings.java
@@ -1,10 +1,12 @@
 package com.hbm.lib;
 
+// TODO: main menu fix, ad more memes, add o7 new musics, iv bag revert
+
 public class RefStrings {
 	public static final String MODID = "hbm";
 	public static final String NAME = "Faszom HBM";
 	public static final String VERSION = "2.1-BETA";
-	public static final String CHANGELOG = "- New vomit sounds.$- New cough sounds.$- Dark UI (might revert, gotta ask opinions).$- Custom paintings (replacing default ones).$- Custom main menu (W.I.P.).$- Fixed grammatical errors in en-us.lang.$- Renamed 'screm' to 'sceam' in the files (it was annoying to see).$- Credits update in mcmod.info file.$- More memes.$- Some directories renamed.";
+	public static final String CHANGELOG = "- New vomit sounds.$- New cough sounds.$- Dark UI (might revert, gotta ask opinions).$- Custom paintings (replacing default ones).$- Custom main menu (W.I.P.).$- Fixed grammatical errors in en_us.lang.$- Renamed 'screm' to 'scream' in the files.$- Credits update in mcmod.info file.$- More memes.$- Some directories renamed.$- Added more music.$- IV Bag crafting recipe reverted to insulator one.";
 	//HBM's Beta Naming Convention:
 	//V T (X)
 	//V -> next release version
