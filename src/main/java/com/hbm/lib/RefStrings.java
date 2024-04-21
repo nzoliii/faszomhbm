7b70f34871f8e366e53fd18ce8aebae800be7389
nzoliii <nemethzoltanjozsef@gmail.com>
2024-04-21 12:20:48 +0200

    Changelog testing

--- src/main/java/com/hbm/lib/RefStrings.java
+++ src/main/java/com/hbm/lib/RefStrings.java
@@ -5,8 +5,8 @@ package com.hbm.lib;
 public class RefStrings {
 	public static final String MODID = "hbm";
 	public static final String NAME = "Faszom HBM";
-	public static final String VERSION = "2.1-BETA";
-	public static final String CHANGELOG = "- New vomit sounds.$- New cough sounds.$- Dark UI (might revert, gotta ask opinions).$- Custom paintings (replacing default ones).$- Custom main menu (W.I.P.).$- Fixed grammatical errors in en_us.lang.$- Renamed 'screm' to 'scream' in the files.$- Credits update in mcmod.info file.$- More memes.$- Some directories renamed.$- Added more music.$- IV Bag crafting recipe reverted to insulator one.$- Gerald achievements and sounds replaced.";
+	public static final String VERSION = "2.1-BETA-CLTEST";
+	public static final String CHANGELOG = "§a+ New vomit sounds.$+ New cough sounds.$+ Dark UI (might revert, gotta ask opinions).$+ Custom paintings (replacing default ones).$+ Custom main menu (W.I.P.).$+ Added more music.$§e= Fixed grammatical errors in en_us.lang.$= Renamed 'screm' to 'scream' in the files.$= Credits update in mcmod.info file.$- More memes.$= Some directories renamed.$§e- Gerald achievements and sounds replaced.$§c- IV Bag crafting recipe reverted to insulator one.";
 	//HBM's Beta Naming Convention:
 	//V T (X)
 	//V -> next release version
