95076dc6f5382334d375d37b5fa458251c07d4e0
nzoliii <nemethzoltanjozsef@gmail.com>
2024-04-21 12:32:48 +0200

    Changelog testing 3

--- src/main/java/com/hbm/lib/RefStrings.java
+++ src/main/java/com/hbm/lib/RefStrings.java
@@ -7,12 +7,18 @@ public class RefStrings {
 	public static final String NAME = "Faszom HBM";
 	public static final String VERSION = "2.1-BETA-CLTEST";
 	public static final String CHANGELOG = "§a+ New vomit sounds.$§a+ New cough sounds.$§a+ Dark UI (might revert, gotta ask opinions).$§a+ Custom paintings (replacing default ones).$§a+ Custom main menu (W.I.P.).$§a+ Added more music.$§a+ More memes.$§e= Fixed grammatical errors in en_us.lang.$§e= Renamed 'screm' to 'scream' in the files.$§e= Credits update in mcmod.info file.$§e= Some directories renamed.$§e= Gerald achievements and sounds replaced.$§c- IV Bag crafting recipe reverted to insulator one.";
+
 	//HBM's Beta Naming Convention:
 	//V T (X)
 	//V -> next release version
 	//T -> build type
 	//X -> days since 10/10/10
 	//Drillgon200: I completely ignored this to make my own even worse naming system. Sigh.
+
+	// nzoliii's naming system because why not:
+	// + -> new stuff
+	// = -> edited stuff
+	// - -> removed stuff
 	public static final String CLIENTSIDE = "com.hbm.main.ClientProxy";
 	public static final String SERVERSIDE = "com.hbm.main.ServerProxy";
 }
