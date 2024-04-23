a2248bad5e5dde0d21e067bb261c2af1ca9aa15e
nzoliii <nemethzoltanjozsef@gmail.com>
2024-04-23 12:55:30 +0200

    Update 2.0.2 im an idiot

--- src/main/java/com/hbm/lib/RefStrings.java
+++ src/main/java/com/hbm/lib/RefStrings.java
@@ -9,7 +9,9 @@ package com.hbm.lib;
 public class RefStrings {
 	public static final String MODID = "hbm";
 	public static final String NAME = "Faszom HBM";
-	public static final String VERSION = "2.0.3";
+	public static final String VERSION = "2.0.2";
+
+	// 2.0.2 changelog: public static final String CHANGELOG = "§a+ New vomit sounds.$§a+ New cough sounds.$§a+ Added more music.$§a+ More memes.$§a+ Added old insulator IV Bag crafting recipe.$§e= Fixed grammatical errors in en_us.lang.$§e= Renamed 'screm' to 'scream' in the files.$§e= Credits update in mcmod.info file.$§e= Some directories renamed.$§e= Gerald achievements and sounds replaced.$§e= Power diode max limit changed.";
 	public static final String CHANGELOG = "§a+ New vomit sounds.$§a+ New cough sounds.$§a+ Added more music.$§a+ More memes.$§a+ Added old insulator IV Bag crafting recipe.$§e= Fixed grammatical errors in en_us.lang.$§e= Renamed 'screm' to 'scream' in the files.$§e= Credits update in mcmod.info file.$§e= Some directories renamed.$§e= Gerald achievements and sounds replaced.$§e= Power diode max limit changed.";
 
 	//HBM's Beta Naming Convention:
