59b789ad941ec805a23470bc74994ba8193bbf4d
nzoliii <nemethzoltanjozsef@gmail.com>
2024-07-15 04:05:11 +0200

    changing the changelog

--- src/main/java/com/hbm/lib/RefStrings.java
+++ src/main/java/com/hbm/lib/RefStrings.java
@@ -5,19 +5,18 @@ package com.hbm.lib;
 //		Add o7's new music
 //		Dark UI as a resource pack
 //		Diarrhea mechanics finally?
-//		Refine puke sounds to have splashy sound.
-//		Pegasi and Missile silo achievement typo fix.
-//		Oppenheimer meme before gadget explosion.
 //		Fix antimatter barrel scale (make it 5x5 from 4x4)
 //		Infintie barell which destroys a lot of fluid in a second (fluid destroyer)
+//		Citadel structure from hl2 lol, containing a dfc thats melting down lol
+//		Borealis structure from hl2, containing WMD's
 
 public class RefStrings {
 	public static final String MODID = "hbm";
 	public static final String NAME = "Faszom HBM";
-	public static final String VERSION = "2.1-BETA";
+	public static final String VERSION = "2.1-BETA-COMBINE";
 
 	// 2.0.2 changelog: public static final String CHANGELOG = "§a+ New vomit sounds.$§a+ New cough sounds.$§a+ Added more music.$§a+ More memes.$§a+ Added old insulator IV Bag crafting recipe.$§e= Fixed grammatical errors in en_us.lang.$§e= Renamed 'screm' to 'scream' in the files.$§e= Credits update in mcmod.info file.$§e= Some directories renamed.$§e= Gerald achievements and sounds replaced.$§e= Power diode max limit changed.";
-	public static final String CHANGELOG = "§a+ Added some surprises.";
+	public static final String CHANGELOG = "§e= Combine occupation of Earth.";
 
 	//HBM's Beta Naming Convention:
 	//V T (X)
@@ -27,9 +26,9 @@ public class RefStrings {
 	//Drillgon200: I completely ignored this to make my own even worse naming system. Sigh.
 
 	// nzoliii's naming system because why not:
-	// + -> new stuff
-	// = -> edited stuff
-	// - -> removed stuff
+	// §A+ -> new stuff
+	// §e= -> edited stuff
+	// §C- -> removed stuff
 	public static final String CLIENTSIDE = "com.hbm.main.ClientProxy";
 	public static final String SERVERSIDE = "com.hbm.main.ServerProxy";
 }
